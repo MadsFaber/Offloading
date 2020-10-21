f658a1933fdad442ee100eca97f80b3562134079
Mads <madsfaber111@live.dk>
2020-10-21 10:50:18 +0200

    Implementet socekts to send a file

--- src/main/java/Network/Server.java
+++ src/main/java/Network/Server.java
@@ -5,25 +5,28 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import database.Datamanager;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.SQLException;
 
 public class Server {
 
     public void start() throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress("192.168.43.68",8080), 0);
+    /*    HttpServer server = HttpServer.create(new InetSocketAddress("172.28.223.149", 8080), 0);
         HttpContext context = server.createContext("/");
         context.setHandler(Server::handleRequest);
-        server.start();
+        server.start();*/
+        socketMethod();
     }
 
-    private static void handleRequest(HttpExchange exchange) throws IOException {
+  /*  private static void handleRequest(HttpExchange exchange) throws IOException {
         try {
             String response = Datamanager.getFirstDbname();
             System.out.println(exchange.getRemoteAddress());
             exchange.sendResponseHeaders(200, response.getBytes().length);
+
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
@@ -31,11 +34,48 @@ public class Server {
             throwables.printStackTrace();
         }
 
+    }*/
 
+    public final static int SOCKET_PORT = 13267;  // you may change this
+    public final static String FILE_TO_SEND = "C:/Programming/Offloading/TestFiles/test.txt";  // you may change this
 
-
-
+    public static void socketMethod() throws  IOException{
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        OutputStream os = null;
+        ServerSocket servsock = null;
+        Socket sock = null;
+        try {
+            servsock = new ServerSocket(SOCKET_PORT);
+            while (true) {
+                System.out.println("Waiting...");
+                try {
+                    sock = servsock.accept();
+                    System.out.println("Accepted connection : " + sock);
+                    // send file
+                    File myFile = new File (FILE_TO_SEND);
+                    byte [] mybytearray  = new byte [(int)myFile.length()];
+                    fis = new FileInputStream(myFile);
+                    bis = new BufferedInputStream(fis);
+                    bis.read(mybytearray,0,mybytearray.length);
+                    os = sock.getOutputStream();
+                    System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+                    os.write(mybytearray,0,mybytearray.length);
+                    os.flush();
+                    System.out.println("Done.");
+                }
+                finally {
+                    if (bis != null) bis.close();
+                    if (os != null) os.close();
+                    if (sock!=null) sock.close();
+                }
+            }
+        }
+        finally {
+            if (servsock != null) servsock.close();
+        }
     }
-
 }
+
+
 
