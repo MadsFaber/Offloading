1f337ed18f7d17eee7a063684f0f8e7dadcccf6c
Philip Holler <philipholler94@icloud.com>
2020-10-28 13:05:11 +0100

    Works on MacOS and Laurits computer

--- src/main/java/p7gruppe/p7/offloading/controller/JobsController.java
+++ src/main/java/p7gruppe/p7/offloading/controller/JobsController.java
@@ -1,7 +1,32 @@
 package p7gruppe.p7.offloading.api;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.context.request.NativeWebRequest;
+import p7gruppe.p7.offloading.model.Job;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class JobsController implements JobsApi {
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        System.out.println("Got get request");
+        return Optional.empty();
+    }
 
+    @Override
+    public ResponseEntity<List<Job>> jobsGet(@NotNull @Valid Job query) {
+        System.out.println("Got get request 2");
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> jobsPost(@NotNull @Valid Job query) {
+        System.out.println("Got post request");
+        System.out.println("Received post - id: " + query.getId() + ", name: " + query.getName());
+        return null;
+    }
 }
